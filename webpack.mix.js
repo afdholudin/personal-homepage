bd6c8c028f3c5f75aeafa2860edd932731b65df9
afdholudin <afdholudin213@gmail.com>
2022-05-07 19:17:42 +0700

    Change CDN to local files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,10 @@
 const mix = require('laravel-mix');
 require("laravel-mix-purgecss");
 
-
-
+// mix.webpackConfig({
+//     stats: {
+//         children: true,
+//     },
+// });
 
 mix.sass('sass/style.scss', 'public/css');
