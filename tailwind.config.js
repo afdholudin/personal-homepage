aab0ccfc5720811c79c484ef19de02a5798c96c8
Afdholudin <afdholudin213@gmail.com>
2022-08-11 15:19:23 +0700

    Finishing last section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,21 @@ module.exports = {
     "index.html"
   ],
   theme: {
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+      },
+    },
     extend: {
       colors:{
         'primary' : '#048FF2',
         'secondary' : '#0378CC',
         'tertiary' : '#1DC8F0',
         'primary-dark': '#131718',
-        'second-sect': '#262225'
+        'second-sect': '#262225',
+        'last-sect': '#3D3A3B'
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
